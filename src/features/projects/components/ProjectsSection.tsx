dd5334201197ede050386988df89e97b29a64019
Giovanna Esmeralda Chavez Espino <esme.espino19@gmail.com>
2026-06-22 20:29:07 -0600

    feat(projects): add ProjectsSection container for project showcase

---
+++ src/features/projects/components/ProjectsSection.tsx
@@ -0,0 +1,9 @@
+import { ProjectsGrid } from "./ProjectsGrid";
+
+export function ProjectsSection() {
+    return (
+        <section id="projects" className="flex flex-col gap-8">
+            <ProjectsGrid />
+        </section>
+    )
+}

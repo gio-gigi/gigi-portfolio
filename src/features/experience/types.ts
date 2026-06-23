54eee45ebd62bbeebaa9624b8f82b8fe4598b3d0
Giovanna Esmeralda Chavez Espino <esme.espino19@gmail.com>
2026-06-22 20:27:11 -0600

    chore(experience): add ExperienceItem and Course type definitions

---
+++ src/features/experience/types.ts
@@ -0,0 +1,12 @@
+export interface ExperienceItem {
+  role: string;
+  company: string;
+  period: string;
+  bullets: string[];
+}
+
+export interface Course {
+  name: string;
+  org: string;
+  year: string;
+}
